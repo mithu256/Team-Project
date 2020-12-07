97dfbb581b6a729b32baefd00d679242eff9f1a1
anjali678 <nayananjali631@gmai.com>
2020-12-07 19:44:02 +0530

    front end design for hms

--- front_end/resources/views/blade-scafolding/partials/copyright.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-8">
-            <div class="footer-text">
-                <p>© 2020 Lifecare. All Rights Reserved.</p>
-            </div>
-        </div>
-    </div>
-</div>
